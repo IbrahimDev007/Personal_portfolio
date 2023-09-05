375c4b6b03a9669a47c3480eb1691500f8145661
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-09-05 13:25:20 +0600

    image replace

--- src/Constant/constant.js
+++ src/Constant/constant.js
@@ -18,7 +18,7 @@ export const skill = [
 export const project = [
     {
         title: "Triangle Sports Academy",
-        img: './src/assets/project1.png',
+        img: 'project1.png',
         live: "https://spiffy-babka-8a76f1.netlify.app/",
         feture: ["Authentication System", "Online Payments", "Role-Based Dashboards"],
         technology: ["React", "Tailwind CSS", "Firebase", "MongoDB", "Node.js", "Express.js", "JWT", "Stripe"],
@@ -27,7 +27,7 @@ export const project = [
     },
     {
         title: "Supreme Toy Market",
-        img: './src/assets/project 2.png',
+        img: 'project 2.png',
         live: "https://admirable-brioche-aee761.netlify.app/",
         feture: ["Shop by Category", "User Authentication", "Dedicated Toy Pages"],
         technology: ["React", "Tailwind CSS", "Firebase", "MongoDB", "Node.js", "Express.js"],
@@ -36,7 +36,7 @@ export const project = [
     },
     {
         title: "Indian Chef Recipe",
-        img: './src/assets/project 3.png',
+        img: 'project 3.png',
         live: "https://resonant-dieffenbachia-d0dca2.netlify.app/",
         feture: ["Newsletter Section", "User Authentication", "Collapsible Mobile Menu"],
         technology: ["React", "tailwind", "Firebase", "Node.js"],
