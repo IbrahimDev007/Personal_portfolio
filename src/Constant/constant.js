1c2c8608597658289c478fe55178e4ab5e0a2102
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-09-05 13:12:36 +0600

    configuration change for deploy

--- src/Constant/constant.js
+++ src/Constant/constant.js
@@ -1,16 +1,16 @@
 export const skill = [
 
-    { img: "/public/stack/express.png", title: "demo " },
-    { img: "/public/stack/figma logo.jpeg", title: "demo " },
-    { img: "/public/stack/js.PNG", title: "demo " },
-    { img: "/public/stack/mongo.png", title: "demo " },
-    { img: "/public/stack/mui.PNG", title: "demo " },
-    { img: "/public/stack/nextJS.png", title: "demo " },
-    { img: "/public/stack/node.png", title: "demo " },
-    { img: "/public/stack/react.PNG", title: "demo " },
-    { img: "/public/stack/redux.png", title: "demo " },
-    { img: "/public/stack/tailwind.PNG", title: "demo " },
-    { img: "/public/stack/typescript.png", title: "demo " },
+    { img: "./public/stack/express.png", title: "demo " },
+    { img: "./public/stack/figma logo.jpeg", title: "demo " },
+    { img: "./public/stack/js.PNG", title: "demo " },
+    { img: "./public/stack/mongo.png", title: "demo " },
+    { img: "./public/stack/mui.PNG", title: "demo " },
+    { img: "./public/stack/nextJS.png", title: "demo " },
+    { img: "./public/stack/node.png", title: "demo " },
+    { img: "./public/stack/react.PNG", title: "demo " },
+    { img: "./public/stack/redux.png", title: "demo " },
+    { img: "./public/stack/tailwind.PNG", title: "demo " },
+    { img: "./public/stack/typescript.png", title: "demo " },
 
 ]
 
